4efb1848393c6351b6037abeee993c01f782155d
LuBingtan <LuBingtan@users.noreply.github.com>
2026-05-07 06:26:49 +0000

    deploy: 93c6ac59bb651be1d42fed815d3031b9fd932eff

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f00bd26b.js',
+                path_to_root + 'searchindex-4a127a49.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
