7eec0a8b44cefb9583ece913b2a9d2cbfe2f2f78
LuBingtan <LuBingtan@users.noreply.github.com>
2026-05-20 05:57:31 +0000

    deploy: c0c441fd188aae1f5ee18075089b47500cae9512

--- theme/goatcounter-5a31bc19.js
+++
@@ -1,9 +0,0 @@
-// GoatCounter — privacy-friendly page view analytics
-// Sign up at https://www.goatcounter.com and replace YOUR-CODE below.
-// (function() {
-//   var script = document.createElement('script');
-//   script.src = 'https://gc.zgo.at/count.js';
-//   script.setAttribute('data-goatcounter', 'https://YOUR-CODE.goatcounter.com/count');
-//   script.async = true;
-//   document.head.appendChild(script);
-// })();
